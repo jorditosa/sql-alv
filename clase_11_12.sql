f9c697756553fd0de1e4250fe5d7b1bb3908642a
jorditosa <jordisato88@gmail.com>
2023-12-11 19:42:19 +0100

    select practica

--- clase_11_12.sql
+++ clase_11_12.sql
@@ -1,8 +1,3 @@
-mysql> USE alv_proy;
-Reading table information for completion of table and column names
-You can turn off this feature to get a quicker startup with -A
-
-Database changed
 mysql> SHOW TABLES;
 +--------------------+
 | Tables_in_alv_proy |
@@ -139,3 +134,59 @@ mysql> SELECT * FROM bikes;
 +----+---------+--------+--------+
 2 rows in set (0,00 sec)
 
+mysql> DESCRIBE users;
++-------------+-------------+------+-----+---------+----------------+
+| Field       | Type        | Null | Key | Default | Extra          |
++-------------+-------------+------+-----+---------+----------------+
+| id          | int         | NO   | PRI | NULL    | auto_increment |
+| name        | varchar(40) | YES  |     | NULL    |                |
+| email       | varchar(60) | NO   | UNI | NULL    |                |
+| userCreated | datetime    | YES  |     | NULL    |                |
+| phone       | int         | NO   |     | NULL    |                |
++-------------+-------------+------+-----+---------+----------------+
+5 rows in set (0,00 sec)
+
+mysql> INSERT INTO users (name, email, userCreated, phone) VALUES ('Juan', 'usuario5@example.com', 2023-12-01, 151919);
+ERROR 1292 (22007): Incorrect datetime value: '2010' for column 'userCreated' at row 1
+mysql> INSERT INTO users (name, email, userCreated, phone) VALUES ('Juan', 'usuario5@example.com', '2023-12-01', 151919);
+Query OK, 1 row affected (0,01 sec)
+
+mysql> SELECT * FROM users WHERE IS LIKE 'Alvaro';
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'IS LIKE 'Alvaro'' at line 1
+mysql> SELECT * FROM users WHERE name LIKE 'Alvaro';
++----+--------+----------------------+---------------------+-----------+
+| id | name   | email                | userCreated         | phone     |
++----+--------+----------------------+---------------------+-----------+
+|  5 | Alvaro | usuario1@example.com | 2023-12-11 10:00:00 | 123456789 |
++----+--------+----------------------+---------------------+-----------+
+1 row in set (0,00 sec)
+
+mysql> SELECT * FROM users;
++----+--------+----------------------+---------------------+-----------+
+| id | name   | email                | userCreated         | phone     |
++----+--------+----------------------+---------------------+-----------+
+|  5 | Alvaro | usuario1@example.com | 2023-12-11 10:00:00 | 123456789 |
+|  6 | Jordi  | usuario2@example.com | 2023-12-12 11:30:00 | 987654321 |
+|  7 | Marta  | usuario3@example.com | 2023-12-13 09:45:00 | 555555555 |
+|  9 | Miriam | usuario4@example.com | 2023-12-05 00:00:00 | 123456789 |
+| 10 | Juan   | usuario5@example.com | 2023-12-01 00:00:00 |    151919 |
++----+--------+----------------------+---------------------+-----------+
+5 rows in set (0,00 sec)
+
+mysql> SELECT phone FROM users WHERE LENGTH(phone) BETWEEN 0 AND 8;
++--------+
+| phone  |
++--------+
+| 151919 |
++--------+
+1 row in set (0,00 sec)
+
+mysql> SELECT * FROM users WHERE LENGTH(phone) BETWEEN 0 AND 8;
++----+------+----------------------+---------------------+--------+
+| id | name | email                | userCreated         | phone  |
++----+------+----------------------+---------------------+--------+
+| 10 | Juan | usuario5@example.com | 2023-12-01 00:00:00 | 151919 |
++----+------+----------------------+---------------------+--------+
+1 row in set (0,00 sec)
+
+mysql> 
